a7f5a162fec1aa279a2d0640850671ec3de4eb65
Snehasish Dhar <58779604+dsnehasish74@users.noreply.github.com>
2020-07-24 22:05:32 +0530

    Create books.js

---
+++ src/shared/books.js
@@ -0,0 +1,49 @@
+export const books=[
+  {
+    id:0,
+    course:"1st year",
+    sub:[
+      {
+        title:"Math",
+        books:[{link:"https://drive.google.com/file/d/1SCOmFGDqBL3jvVcx3dEebe0bHlwmZkBJ/view?usp=sharing",name:"Mathematics by Dr. Guruprasad Samanta"},
+               {link:"https://drive.google.com/file/d/186aaegRK0kN3KdmoOxyL0MTyXh47Ezqa/view?usp=sharing",name:"Diiferantial Calculas by BC Das,BN Mukherjee"},
+               {link:"https://drive.google.com/file/d/1x-FXtMM87Pe3u1CZ9cHACPfMYSc-RMaE/view?usp=sharing",name:"Integral and Differantial Calculus"}
+      ]
+      },
+      {
+        title:"Basic Electrical Engineering",
+        books:[{link:"https://drive.google.com/file/d/1NZfIAv6HXeunPbmuhJDndVTstcUAQDlr/view?usp=sharing",name:"A Course in Electrical Engineering by Chester L. Dawes"},
+               {link:"https://drive.google.com/file/d/1Wsy8MC-88Z_uQJnbHN1ZadMlYv4Qfm1O/view?usp=sharing",name:"BEE by Charavarty,Nath,Chanda"}
+              ]
+      },
+      {
+        title:"Physics",
+        books:[{link:"https://drive.google.com/file/d/1IKs4YyrjE7hpwG4-e5sW_qhhxdOa9Z8n/view?usp=sharing",name:"Vector Analysis by Spiegel"},
+               {link:"https://drive.google.com/file/d/1dSAx2s52rsRZtMVSmfpM_MbeHOJ539qV/view?usp=sharing",name:"Physics by BK Guha"}
+              ]
+      },
+      {
+        title:"Computer",
+        books:[{link:"https://drive.google.com/file/d/1ev93lka9VBjP-V1tl648sp-emlHGp-zC/view?usp=sharing",name:"C book"},
+              ]
+      },
+      {
+        title:"Mechanics",
+        books:[{link:"https://drive.google.com/file/d/1c7M0JNlwT7XXTTGXY3jWYT0UnsEeHeEV/view?usp=sharing",name:"Statics by J.L.Mariam"},
+              ]
+      },
+      {
+        title:"Environment and Ecology",
+        books:[{link:"https://drive.google.com/file/d/11vqauC-gdWYwmbyPsualuQ3Z8svXQPjZ/view?usp=sharing",name:"Environmental Engineering text book"},
+              ]
+      },
+      {
+        title:"Engineering Drawing",
+        books:[{link:"https://drive.google.com/file/d/1DaJFuHUWD3G109CiP3n88YEwPlSj2-D9/view?usp=sharing",name:"Engineering Drawing by ND Bhatt"},
+              ]
+      },
+
+    ]
+  },
+
+]
