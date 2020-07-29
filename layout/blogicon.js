78ec43abf985d67bc3c9e7275424840879f5bb41
Snehasish Dhar <58779604+dsnehasish74@users.noreply.github.com>
2020-07-29 11:31:09 +0530

    Delete blogicon.js

--- layout/blogicon.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-function Blogicon(props){
-  return (
-  <a href="https://yournewabode.blogspot.com/"><div className="blog-icon"><i className="fab fa-blogger-b fa-lg "></i></div></a>
-  );
-}
-
-export default Blogicon;
