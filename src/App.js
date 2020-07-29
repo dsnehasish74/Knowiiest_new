b70c36d31b56bc6eeac068dff6dcd91753679dcc
Snehasish Dhar <58779604+dsnehasish74@users.noreply.github.com>
2020-07-29 11:34:47 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
-import React from 'react';
-import logo from './WhatsApp Image 2020-07-20 at 00.14.14.jpeg';
-import './App.css';
+import React,{Component} from 'react';
+import {BrowserRouter,Switch,Route,Redirect} from 'react-router-dom'
+import Navigation from "./layout/navbar.js"
+import Footer from "./layout/footer.js"
+import Blogicon from "./layout/blogicon.js"
+import Home from './pages/home.js'
+import About from './pages/about.js'
+import Books from './pages/books.js'
+import Buildings from './pages/buildings.js'
+import BusRoutes from './pages/busroutes.js'
+import Canteen from './pages/canteen.js'
+import Grade from './pages/grade.js'
+import Clubs from './pages/clubs.js'
+import Tourist from './pages/tourist.js'
+import Fest from './pages/fest.js'
+import Details from './pages/details.js'
+import Buildingdetails from './pages/buildingdetails.js'
+import TouristDetails from './pages/touristdetails.js'
+import Festsdetail from './pages/festdetail.js'
+import Canteendetails from './pages/canteendetail.js'
 
-function App() {
+class App extends Component {
+  render(){
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <br/>
-        <p>
-            This site is temporarily unavailable due to technical issues
-        </p>
-        <p>you can still visit our blog....<a href="https://yournewabode.blogspot.com/" target="blank">click here to visit our blog</a></p>
-      </header>
+    <Navigation/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route  path="/about" component={About}/>
+        <Route  path="/books" component={Books}/>
+        <Route  exact path="/buildings" component={Buildings}/>
+        <Route  exact path="/canteen" component={Canteen}/>
+        <Route  exact path="/busroutes" component={BusRoutes}/>
+        <Route  path="/grade" component={Grade}/>
+        <Route  path="/clubs" component={Clubs}/>
+        <Route  exact path="/tourist" component={Tourist}/>
+        <Route  exact path="/fest" component={Fest}/>
+        <Route path="/busroutes/:id" component={Details}/>
+        <Route path="/buildings/:id" component={Buildingdetails}/>
+        <Route path="/tourist/:id" component={TouristDetails}/>
+        <Route path="/fest/:id" component={Festsdetail}/>
+        <Route path="/canteen/:id" component={Canteendetails}/>
+        <Redirect to="/"/>
+      </Switch>
+      <Footer/>
+      <Blogicon/>
     </div>
+    </BrowserRouter>
   );
+}
 }
 
 export default App;
