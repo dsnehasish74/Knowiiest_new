9e9eceb17fb9382c1536381fe96ae60cdf168dc4
Snehasish Dhar <58779604+dsnehasish74@users.noreply.github.com>
2020-07-29 11:31:59 +0530

    Delete touristintro.js

--- intro/touristintro.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-function TouristIntro(props){
-  return(
-    <div className="touristintro">
-      <h1 className="intro-text">{props.intro}</h1>
-    </div>
-  );
-}
-export default TouristIntro;
