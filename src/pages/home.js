e8cefc6c2dc16b76088e8c13c463ecb697233b35
Snehasish Dhar <dsnehasish74@gmail.com>
2020-10-24 21:03:57 +0530

    feature section added

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Col from '../svg/col.svg'
 import '../css/home.css'
+import logo1 from "../images/book.jpg"
+import logo2 from "../images/blog.jpg"
+import logo3 from "../images/partner.jpg"
+import {Link} from 'react-router-dom'
 
 function Home(){
   return(
@@ -14,8 +18,64 @@ function Home(){
           <img src={Col} alt="iiest"></img>
         </div>
      </div>
-    <div className="feature_section">
+    <div className="feature_section container">
     <h2>Our Top Features</h2>
+    <div className="feature_card_container">
+    <div className="feature_card">
+    <div className="feature_card_img_container">
+      <img className="feature_card_img" src={logo1} alt="book"></img>
+      <div class="overlay">
+        <div class="text">
+          <h4>Explore this Feature</h4>
+          <p>Since it's establishment in 1856, the Indian Institute of Engineering Science and Technology, Shibpur (IIEST Shibpur) erstwhile BE College</p>
+          </div>
+     </div>
+      </div>
+      <div className="feature_card_text">
+      <h3>Books For Semester Exam</h3>
+      <p>
+        You can find a lot of books related to Semester Exams here
+      </p>
+      <Link to="/books" className="explore_link">explore here</Link>
+    </div>
+    </div>
+    <div className="feature_card">
+    <div className="feature_card_img_container">
+      <img className="feature_card_img" src={logo2} alt="book"></img>
+      <div class="overlay">
+        <div class="text">
+          <h4>Explore this Feature</h4>
+          <p>Since it's establishment in 1856, the Indian Institute of Engineering Science and Technology, Shibpur (IIEST Shibpur) erstwhile BE College</p>
+          </div>
+     </div>
+      </div>
+    <div className="feature_card_text">
+      <h3>Blogs on Campus Life</h3>
+      <p>
+        You can find a lot of books related to Semester Exams here
+      </p>
+      <a href="https://yournewabode.blogspot.com/" className="explore_link">explore here</a>
+    </div>
+    </div>
+    <div className="feature_card">
+    <div className="feature_card_img_container">
+      <img className="feature_card_img" src={logo3} alt="book"></img>
+      <div class="overlay">
+        <div class="text">
+          <h4>Explore this Feature</h4>
+          <p>Since it's establishment in 1856, the Indian Institute of Engineering Science and Technology, Shibpur (IIEST Shibpur) erstwhile BE College</p>
+          </div>
+     </div>
+      </div>
+    <div className="feature_card_text">
+      <h3>Explore other Projects made by IIEST students</h3>
+      <p>
+        You can find a lot of books related to Semester Exams here
+      </p>
+      <Link to="/" className="explore_link">explore here</Link>
+    </div>
+    </div>
+    </div>
     </div>
   </div>
 
