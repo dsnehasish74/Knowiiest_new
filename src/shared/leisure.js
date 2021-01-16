ba6d9b89972ded89b0c985ec072d3685612e7378
Anishde85 <59112660+Anishde85@users.noreply.github.com>
2021-01-16 16:21:28 +0530

    Add files via upload

---
+++ src/shared/leisure.js
@@ -0,0 +1,42 @@
+import logo1 from "../images/gym.jpg"
+import logo2 from "../images/swim.jpg"
+import logo3 from "../images/basket.jpg"
+import logo4 from "../images/lords.jpg"
+import logo5 from "../images/oval.jpg"
+export const Leisure=[
+  {
+    id:0,
+    src:logo1,
+    heading:"Gymnasium",
+    body:"The institute has a Gynmasium for the students. Good modern instruments are there and time to time renovated as per requirement of the candidates. Gynmasium is quiet popular in the institute and students comes here in the evening after their classes and in the morning before the classes. Body Building Championship is also held in the college",
+    alt:"Gynmnasium"
+  },
+    {
+    id:1,
+    src:logo2,
+    heading:"Swimming Pool",
+    body:"The Alumni Swimming Pool is a wonderful gift from our alumni assocation, GAABESU, to the residents of the institute. It started operating from April 2013. It is used for Swimming & Water Polo.",
+    alt:"Swimming Pool"
+  },
+  {
+    id:2,
+    src:logo3,
+    heading:"Basketball Court" ,
+    body:"A basketball court is also present within the campus. 12th of march was indeed the most awaited day for all the basket ball players, the new basket ball court was inaugurated by honorable director of iiest shibpur prof. parthasarathi chakrabarti dedicated to alma mater by B.E.-1991 and B.Arch-1992 alumni batch. ",
+    alt:"Basketball Court"
+  },
+  {
+    id:3,
+    src:logo4,
+    heading:"Lords Ground" ,
+    body:"A basketball court is also present within the campus. 12th of march was indeed the most awaited day for all the basket ball players, the new basket ball court was inaugurated by honorable director of iiest shibpur prof. parthasarathi chakrabarti dedicated to alma mater by B.E.-1991 and B.Arch-1992 alumni batch. ",
+    alt:"Lords Ground"
+  },
+  {
+    id:4,
+    src:logo5,
+    heading:"Oval Ground" ,
+    body:"A basketball court is also present within the campus. 12th of march was indeed the most awaited day for all the basket ball players, the new basket ball court was inaugurated by honorable director of iiest shibpur prof. parthasarathi chakrabarti dedicated to alma mater by B.E.-1991 and B.Arch-1992 alumni batch. ",
+    alt:"Oval Ground"
+  }
+]
